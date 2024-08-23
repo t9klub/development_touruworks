536e3d30e41468e772449cc3284dfb57bc39819a
Alhuft <xnotes7@gmail.com>
2024-08-23 23:38:08 +0700

    add cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
                 background-color: #171618;
             }
         </style> 
-
+ <script src="https://cdn.tailwindcss.com"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased"> 
