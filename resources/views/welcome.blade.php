c9ee82eae10db3597d80108fc0e5b77e0e1b7fb8
Alhuft <xnotes7@gmail.com>
2024-09-02 18:54:39 +0700

    added link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased"> 
-        <div class="h-screen flex items-center justify-center mx-4">
+        <div class="h-screen flex items-center justify-center mx-4 mb-4">
             <div class="w-full md:w-10/12 lg:w-3/4 xl:w-1/3  animation-all duration-1000 scale-95" >
                 <div class="w-full overflow-hidden text-neutral-600 dark:divide-neutral-100 dark:bg-neutral-900/50 dark:text-neutral-300 px-4 py-2">
                     <div class="flex flex-col">
@@ -55,7 +55,7 @@
                             <div class="absolute inset-0 flex items-center flex-col justify-center z-10">
                                 <small>Development</small>
                                 <h1 class="font-bold text-xl text-white -mt-2 ">Touru Works</h1>
-                                <small>Give me a powa here <a href="https://touruworks.com" class="underline">Ko-fi</a> or <a href="" class="underline">Trakteer</a></small>
+                                <small>Give me a powa here <a href="https://ko-fi.com/touru9klub" class="underline">Ko-fi</a> or <a href="https://trakteer.id/touru" class="underline">Trakteer</a></small>
                             </div>
                         </div>
                       
