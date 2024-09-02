af9bdd1aaa0e329329e365e61f1b84faa8181d11
Alhuft <xnotes7@gmail.com>
2024-09-02 22:02:05 +0700

    add google index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,7 @@
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
         <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/tabs.js"></script>
+        <meta name="google-site-verification" content="I3Jpqomt_cy729MCwFtp10xD5QRW3UwJEIPE9ZC6gnA" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased"> 
@@ -56,11 +57,11 @@
                 <div class="w-full overflow-hidden text-neutral-600 dark:divide-neutral-100 dark:bg-neutral-900/50 dark:text-neutral-300 px-4 py-2">
                     <div class="flex flex-col">
                         <div class="relative h-[160px] w-full overflow-hidden rounded-md mb-4">
-                            <img src="https://media1.tenor.com/m/uNb4xaRRK9UAAAAd/bangboo-snif.gif" alt="Deskripsi gambar" class="absolute inset-0 object-cover w-full h-full">
+                            <img src="https://media1.tenor.com/m/uNb4xaRRK9UAAAAd/bangboo-snif.gif" alt="header_bangboo_sniff" class="absolute inset-0 object-cover w-full h-full">
                             <div class="absolute inset-0 flex items-center flex-col justify-center z-10">
                                 <small>Development</small>
                                 <h1 class="font-bold text-xl text-white -mt-2 ">Touru Works</h1>
-                                <small>Give me a powa here <a href="https://ko-fi.com/touru9klub" class="underline">Ko-fi</a> or <a href="https://trakteer.id/touru" class="underline">Trakteer</a></small>
+                                <small>Give me a powa here <a href="https://ko-fi.com/touru9klub" class="underline">Ko-fi</a> or <a href="https://trakteer.id/touruworks" class="underline">Trakteer</a></small>
                             </div>
                         </div>
                       
@@ -77,7 +78,7 @@
                         <div x-cloak x-show="isExpanded" id="accordionItemOne" role="region" aria-labelledby="controlsAccordionItemOne" x-collapse x-transition>
                             <div class="p-4 text-sm sm:text-base text-pretty">
                                 Website ini dirancang untuk membantu pengembangan ultilitas untuk keperluan komisi yang kita jalanin, beserta menjadi dokumentasi catatan pengembangan. <br><br>
-                                Semua yang kita kembangkan 100% gratis tanpa iklan untuk kalian. jika kalian ada pertanyaan bisa ditanyakan di x  <a href="#" class="underline underline-offset-2 text-black dark:text-white">@touru9klub</a> atau pada discord  <a href="#" class="underline underline-offset-2 text-black dark:text-white">tourubaskara</a>.
+                                Semua yang kita kembangkan 100% gratis tanpa iklan untuk kalian. jika kalian ada pertanyaan bisa ditanyakan di x  <a href="https://x.com/touru9klub" class="underline underline-offset-2 text-black dark:text-white hover:text-yellow-400 transition-all duration-300">@touru9klub</a> atau pada discord  <a href="https://discord.com/users/1009807052882841701" class="underline underline-offset-2 text-black dark:text-white hover:text-yellow-400 transition-all duration-300">tourubaskara</a>.
                             </div>
                         </div>
                     </div>
@@ -107,7 +108,7 @@
                     <div x-data="{ isExpanded: false }" class="divide-y  divide-white/10 dark:divide-white/10  rounded-lg bg-card/60 shadow-md shadow-white/5 group transition-all duration-600 mt-4"">
                         <button id="controlsAccordionItemThreee" type="button" class="flex w-full items-center justify-between gap-4 bg-neutral-50 p-4 text-left underline-offset-2 hover:bg-neutral-50/75 focus-visible:bg-neutral-50/75 focus-visible:underline focus-visible:outline-none dark:bg-card dark:hover:bg-card/75 dark:focus-visible:bg-neutral-900/75 transition-all duration-500 group-hover:text-primary bg-[url('{{asset('/src/png/dot_pattern.png')}}')] bg-right-top bg-no-repeat bg-blend-exclusion" aria-controls="accordionItemOne" @click="isExpanded = ! isExpanded" :class="isExpanded ? 'text-onSurfaceStrong dark:text-onSurfaceDarkStrong font-bold rounded-t-lg text-yellow-500'  : 'text-onSurface dark:text-onSurfaceDark font-medium rounded-lg'" :aria-expanded="isExpanded ? 'true' : 'false'">
                             <div class="inline-flex items-center space-x-2">
-                                <p>Auto Load Url Livechat Youtube</p>
+                                <p>Auto Load Livechat Youtube</p>
                             </div>
                             
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke="currentColor" class="size-5 shrink-0 transition" aria-hidden="true" :class="isExpanded  ?  'rotate-180'  :  ''">
@@ -137,14 +138,14 @@
                                     <li class="relative flex-1 ">
                                         <div class="flex items-start font-medium w-full">
                                         <span
-                                        class="w-6 h-6 bg-gray-50 border-2 relative z-10 border-gray-200 rounded-full flex justify-center items-center mr-3 text-sm  lg:w-10 lg:h-10">2</span>
+                                        class="w-6 h-6 aspect-square bg-gray-50 border-2 relative z-10 border-gray-200 rounded-full flex justify-center items-center mr-3 text-sm  lg:w-10 lg:h-10">2</span>
                                             <div class="flex flex-col">
                                                 <h4 class="text-base font-semibold text-primary mb-2">Paste Url Profile Youtube</h4>
                                                 <p class="text-sm text-gray-600 max-w-lg">
                                                     Silahkan paste url profile livechat kalian pada kolom dibawah
                                                 </p> 
                                                 <div class="relative z-20 flex items-center mb-2">
-                                                <input id="url_input" class="p-2 text-gray border rounded mr-4  w-52 lg:w-96 focus:outline-none bg-gray-200" placeholder="Put your youtube url here..." >
+                                                <input id="url_input" class="placeholder:text-gray/50 p-2 text-gray border rounded mr-4 w-52 xl:w-72 focus:outline-none bg-gray-200" placeholder="Put profile youtube url here..." >
                                                 <button id="generate_url" class="flex items-center justify-center h-10 w-10 text-xs bg-white border rounded-md cursor-pointer border-neutral-200/60 hover:bg-neutral-100 active:bg-white focus:bg-white focus:outline-none text-neutral-500 hover:text-neutral-600 group">
                                                 
                                                     {{-- <svg class="w-4 h-4 stroke-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -164,65 +165,56 @@
                                     <li class="relative flex-1 ">
                                         <a class="flex items-start font-medium w-full  ">
                                         <span
-                                        class="w-6 h-6 bg-gray-50 border-2 relative z-10 border-gray-200 rounded-full flex justify-center items-center mr-3 text-sm  lg:w-10 lg:h-10">3</span>
+                                        class="w-6 h-6 aspect-square bg-gray-50 border-2 relative z-10 border-gray-200 rounded-full flex justify-center items-center mr-3 text-sm  lg:w-10 lg:h-10">3</span>
                                             <div class="block">
                                             <h4 class="text-base  font-semibold  text-primary mb-2">Put result url into obs browser</h4>
                                             <p class="text-sm text-gray-600 max-w-xs">
                                                 Setelah kalian menekan <b>Generate</b> maka copy hasil url dibawah ke halaman browser obs kalian
-                                            <div x-data="{
-                                                copyText: '',
-                                                copyNotification: false,
-                                                copyToClipboard() {
-                                                    // Mendapatkan referensi elemen textarea
-                                                    const textarea = document.querySelector('#result-textarea');
-                                                    // Menyalin teks dari textarea ke clipboard
-                                                    if (textarea) {
-                                                        navigator.clipboard.writeText(textarea.value);
-                                                        this.copyNotification = true;
-                                                        let that = this;
-                                                        setTimeout(function(){
-                                                            that.copyNotification = false;
-                                                        }, 3000);
+                                                <div x-data="{
+                                                    copyText: '',
+                                                    copyNotification: false,
+                                                    copyToClipboard() {
+                                                        // Mendapatkan referensi elemen textarea
+                                                        const textarea = document.querySelector('#result-textarea');
+                                                        // Menyalin teks dari textarea ke clipboard
+                                                        if (textarea) {
+                                                            navigator.clipboard.writeText(textarea.value);
+                                                            this.copyNotification = true;
+                                                            let that = this;
+                                                            setTimeout(function(){
+                                                                that.copyNotification = false;
+                                                            }, 3000);
+                                                        }
                                                     }
-                                                }
-                                            }" class="relative z-20 flex items-center mb-2">
-                                            
-                                            <!-- Area Notifikasi Salin -->
-                                            <div x-show="copyNotification" 
-                                                 x-transition:enter="transition ease-out duration-300" 
-                                                 x-transition:enter-start="opacity-0 translate-x-2" 
-                                                 x-transition:enter-end="opacity-100 translate-x-0" 
-                                                 x-transition:leave="transition ease-in duration-300" 
-                                                 x-transition:leave-start="opacity-100 translate-x-0" 
-                                                 x-transition:leave-end="opacity-0 translate-x-2" 
-                                                 class="absolute right-0" 
-                                                 x-cloak>
-                                                <div class="px-3 h-7 -ml-1.5 items-center flex text-xs bg-green-500 border-r border-green-500 -translate-x-full text-white rounded">
-                                                    <span>Copied!</span>
-                                                    <div class="absolute right-0 inline-block h-full -mt-px overflow-hidden translate-x-3 -translate-y-2 top-1/2">
-                                                        <div class="w-3 h-3 origin-top-left transform rotate-45 bg-green-500 border border-transparent"></div>
+                                                }" class="relative z-20 flex items-center mb-2">
+                                                
+                                                <!-- Area Notifikasi Salin -->
+                                                <div x-show="copyNotification" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 translate-x-2" x-transition:enter-end="opacity-100 translate-x-0" x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 translate-x-0" x-transition:leave-end="opacity-0 translate-x-2" class="absolute right-14" style="display: none;">
+                                                    <div class="px-3 h-7 -ml-4 items-center flex text-xs bg-green-500 border-r border-green-500 -translate-x-full text-white rounded">
+                                                        <span>Copied!</span>
+                                                        <div class="absolute right-0 inline-block h-full -mt-px overflow-hidden translate-x-3 -translate-y-2 top-1/2">
+                                                            <div class="w-3 h-3 origin-top-left transform rotate-45 bg-green-500 border border-transparent"></div>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            
-                                            <!-- Button Salin -->
-                                       
-                                            
-                                            <!-- Textarea untuk hasil -->
-                                            <input id="result-textarea" class="placeholder:text-gray/50 p-2 text-gray border rounded mr-4  w-52 lg:w-96 focus:outline-none bg-gray-200" placeholder="https://dev.touruworks.com/load/...." readonly disabled>
-                                            <button @click="copyToClipboard();" class="flex items-center justify-center h-10 w-10 text-xs bg-white border rounded-md cursor-pointer border-neutral-200/60 hover:bg-neutral-100 active:bg-white focus:bg-white focus:outline-none text-neutral-500 hover:text-neutral-600 group">
-                                                <svg x-show="copyNotification" class="w-4 h-4 text-green-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" x-cloak>
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                                                </svg>
-                                                <svg x-show="!copyNotification" class="w-4 h-4 stroke-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <g fill="none" stroke="none">
-                                                        <path d="M7.75 7.757V6.75a3 3 0 0 1 3-3h6.5a3 3 0 0 1 3 3v6.5a3 3 0 0 1-3 3h-.992" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                        <path d="M3.75 10.75a3 3 0 0 1 3-3h6.5a3 3 0 0 1 3 3v6.5a3 3 0 0 1-3 3h-6.5a3 3 0 0 1-3-3v-6.5z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                    </g>
-                                                </svg>
-                                            </button>
-                                            </div>
+                                                
+                                                <!-- Button Salin -->
                                            
+                                                
+                                                <!-- Textarea untuk hasil -->
+                                                <input id="result-textarea" class="placeholder:text-gray/50 p-2 text-gray border rounded mr-4  w-52 lg:w-72 focus:outline-none bg-gray-200" placeholder="https://dev.touruworks.com/load/...." readonly="" disabled="">
+                                                <button @click="copyToClipboard();" class="flex items-center justify-center h-10 w-10 text-xs bg-white border rounded-md cursor-pointer border-neutral-200/60 hover:bg-neutral-100 active:bg-white focus:bg-white focus:outline-none text-neutral-500 hover:text-neutral-600 group">
+                                                    <svg x-show="copyNotification" class="w-4 h-4 text-green-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="display: none;">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+                                                    </svg>
+                                                    <svg x-show="!copyNotification" class="w-4 h-4 stroke-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                        <g fill="none" stroke="none">
+                                                            <path d="M7.75 7.757V6.75a3 3 0 0 1 3-3h6.5a3 3 0 0 1 3 3v6.5a3 3 0 0 1-3 3h-.992" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                            <path d="M3.75 10.75a3 3 0 0 1 3-3h6.5a3 3 0 0 1 3 3v6.5a3 3 0 0 1-3 3h-6.5a3 3 0 0 1-3-3v-6.5z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        </g>
+                                                    </svg>
+                                                </button>
+                                                </div>
                                             <span class="text-sm text-gray-600 max-w-xs"> Finish ✨<br>*cara ini tidak berfungsi untuk streaming unlisted dan membership</span>
                                             
                                             </p>
